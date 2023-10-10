9bf418258c8af233bb9a7f4d64ae8b5a0135da7c
AshwinG-14159 <ashwin03goyal@gmail.com>
2023-10-10 23:56:58 +0530

    polar second run done

--- scripts/get_result_tables_new.py
+++ scripts/get_result_tables_new.py
@@ -23,9 +23,16 @@ t_script_start=time.time()
 set_of_rows_occ = []
 set_of_rows_polar = []
 names_of_regions=['occ', 'occ_free', 'free', 'free_occ']
-gap = 30
-total_attempts = 400
-version=3
+gap = 10
+total_attempts = 100
+version=1
+
+gap2 = 8
+attempts2 = 100
+version2 = 1
+
+plot_version = 5
+
 
 with open('../data/orbitinfo.csv', 'r') as f:
     r = csv.reader(f)
@@ -63,12 +70,12 @@ total_duration_arr = [0,0,0,0]
 total_table = [0,0,0,0]
 
 for row in set_of_rows_occ:
-    print('row: ', row)
-    print('name: ', row[0])
+    # print('row: ', row)
+    # print('name: ', row[0])
     found = 0
     for k_index in range(4):
         table = f"{path_to_tables}/{binning}/{row[0][:-6]}_{row[0][-5:]}_V1.0_{binning}_3_CoincSigmaTable_V2_{names_of_regions[k_index]}_ver{version}.fits"
-        print('table to find: ', table)
+        # print('table to find: ', table)
         try:
             with fits.open(table) as hdul:
                 table=Table(hdul[1].data)
@@ -95,7 +102,7 @@ for row in set_of_rows_occ:
 
 print(f'count of orbits seen: {mark}')
 
-x_vals = [1,2,3,4]
+x_vals = [1,2,3,4,5]
 
 frequencies = []
 
@@ -106,132 +113,96 @@ for k_index in range(4):
             new_arr[element][(int(col)-5)] = total_table[k_index][col][element]/total_duration_arr[k_index]
     frequencies.append(new_arr)
 
-frequencies = np.array(frequencies)
+# frequencies = np.array(frequencies)
 
-print(frequencies)
+# print(frequencies)
 print("total duration arr: ", total_duration_arr)
 
 
 
-plt.figure(figsize=(8,12))
-
-for quads in range(len(frequencies[0])):
-    for threshold in range(len(frequencies[0,0])):
-        plt.plot(x_vals, frequencies[:,quads, threshold], label = str(threshold+5))
-    plt.title(f'Variation of frequency, Num_orbits = {mark}, binning = {binning}, quads = {quads+1}')
-    plt.xticks(x_vals, names_of_regions)
-    plt.yscale('log')
-    plt.xlabel("region")
-    plt.ylabel("frequency of detections")
-    plt.legend()
-    plt.savefig(f'{path_to_plots}/variation_{quads+1}_ver{version}.png')
-    plt.cla()
 
 
-exit()
+# exit()
 
 
-f_timing = open('TGFs_Analysed_durations_polar.txt', 'r')
-data = f_timing.read().split('\n\n')
-# print(data)
-f_timing.close()
 
-mark = 0
+with open('../data/orbitinfo.csv', 'r') as f:
+    r = csv.reader(f)
+    count=0
+    for row in r:
 
+        if(count<1):
+            count+=1
+            continue
+
+        if(float(row[5])<-65 or float(row[5])>65) and row[0][-6:]!='level2':
+            if(count%gap2!=0):
+                count+=1
+                continue
+            set_of_rows_polar.append(row)
+            count+=1
+            if(count/gap2>attempts2): # leave 10 between any 2 and take a total of 200. Binning = 0.001
+                break
+
+
+print('num of polar attempts: ', len(set_of_rows_polar))
 total_duration_arr = 0
 total_table = 0
-
+mark2=0
 for row in set_of_rows_polar:
-    # print('row: ', row)
-    # print('name: ', row[0])
     found = 0
-    # for k_index in range(4):
-    table = f"tables/0.001/{row[0][:-6]}_{row[0][-5:]}_V1.0_0.001_3_CoincSigmaTable_V2_polar.fits"
-    # print('table to find: ', table)
+    table = f"{path_to_tables}/{binning}/{row[0][:-6]}_{row[0][-5:]}_V1.0_{binning}_3_CoincSigmaTable_V2_polar_ver{version2}.fits"
+
     try:
         with fits.open(table) as hdul:
             table=Table(hdul[1].data)
+            duration = hdul[1].header['DURATION']
     except:
-        # print('table not found')
+        # print(table, "not found")
         continue
 
-    # print(table)
     if(type(total_table)==int):
         total_table=table
     else:
         for col in total_table.colnames:
             total_table[col]+= table[col]
-    # print(table)
     found = 1
-    
-    duration = data[mark].split('\n')
-    while(len(duration)==1):
-        mark+=1
-        duration = data[mark].split('\n')
-    print('file name in txt: ', duration[0][34:71], duration[0][83:88])
-    if(duration[0][34:71]+'_'+ duration[0][83:88] != row[0]):
-        print('something is wrong here')
-        exit(0)
-    if(len(duration)>1):
-        d_val = int(duration[1][duration[1].find(':')+1:-1])
-        total_duration_arr+=d_val
-    else:
-        print('number of durations here: ', len(duration), duration)
-        print('duration: ', d_val)
-    
     if(found):
-        mark+=1
-    else:
-        # print("This row's data was never saved")
-        continue
-    # if(mark>10):
-    #     break
+        total_duration_arr+=duration
+        mark2+=1
+print(f'count of orbits seen: {mark2}')
+print(f'total duration of polar: {total_duration_arr}')
 
-print(f'count of orbits seen: {mark-1}')
-
-
-print('total duration: ',total_duration_arr)
-
-
-# for k_index in range(4):
 new_arr = np.zeros((4,6)) #number of quadrants x threshold
 for col in total_table.colnames:
-    # total_table[k_index][col] = total_table[k_index][col].astype(float)
     for element in range(len(total_table[col])):
         new_arr[element][(int(col)-5)] = total_table[col][element]/total_duration_arr
-# frequencies.append(new_arr)
-    # total_table[k_index][col]/=int(total_duration_arr[k_index])
-# frequencies = np.array()
-print(new_arr)
 
 
 frequencies.append(new_arr)
 
 
 frequencies = np.array(frequencies)
-print('fff', frequencies)
-
-
 
 names_of_regions.append("polar")
+
 
 plt.figure(figsize=(8,12))
 
 for quads in range(len(frequencies[0])):
     for threshold in range(len(frequencies[0,0])):
         plt.plot(x_vals, frequencies[:,quads, threshold], label = str(threshold+5))
-        plt.title(f'quads = {quads+1}')
-        plt.xticks(x_vals, names_of_regions)
-        plt.yscale('log')
+    plt.title(f'Variation of frequency, Num_orbits = {mark},{mark2}, binning = {binning}, quads = {quads+1}')
+    plt.xticks(x_vals, names_of_regions)
+    plt.yscale('log')
+    plt.xlabel("region")
+    plt.ylabel("frequency of detections")
     plt.legend()
-    plt.savefig(f'{path_to_plots}/variation_{quads+1}_ver1.png')
+    plt.savefig(f'{path_to_plots}/variation_{quads+1}_ver{plot_version}.png')
     plt.cla()
 
 
 
 
-# print(names_of_regions[k_index], total_table[k_index], total_duration_arr[k_index])
 
-
-
-
+exit(0)
